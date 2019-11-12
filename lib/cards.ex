54f323590b9ea0c4ca442ca95f73c36784a9e4a7
SPrio-kt <supriyo.mahanta@kreeti.com>
2019-11-12 12:15:49 +0530

    load function added with basic case statement

--- lib/cards.ex
+++ lib/cards.ex
@@ -17,9 +17,9 @@ defmodule Cards do
   end
   """
   def create_deck do
-    values =   ["Ace", "Two", "Three","Four","Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-    
+
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
@@ -32,7 +32,7 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
-  
+
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -41,4 +41,22 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "The File does not exists"
+    end
+  end
+
+  # wrong way to check the given file exists or not
+  # def load() do
+  #   {status, binary} = File.read(filename)
+
+  #   if (status === :error) do
+  #     return "Something went wrong"
+  #   end
+  # end
 end
